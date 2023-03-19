a92e25bcc193bd8191ac1f6a0332ac5bc0637a1b
Leandro Leme dos Santos <leandrolemesantos@gmail.com>
2023-03-19 12:46:38 -0300

    76º dia de CTD - Avançando no Projeto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import { useState } from 'react';
+import QRCode from 'react-qr-code';
 import './App.css';
 
 function App() {
+
+  const [link, setLink] = useState('')
+
+  function handleQrcode(event){
+    setLink(event.target.value);
+  }
+
   return (
-    <div classname="container">
+    <div className="container">
       
+    <QRCode
+    value={link}
+    />
+
       <input 
-      classname="input"
-      placeholder="Digite aqui..." />
+      className="input"
+      placeholder="Digite aqui..."
+      value={link}
+      onChange={ (event) => handleQrcode(event)}
+      />
 
     </div>
   );
