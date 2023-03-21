0b96f01fc93e141d39183d20b2880d0c6338b1ae
Leandro Leme dos Santos <leandrolemesantos@gmail.com>
2023-03-20 21:32:39 -0300

    77º dia de CTD - Fase final do projeto

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import { useState } from 'react';
 import QRCode from 'react-qr-code';
+import QRCodeLink from 'qrcode';
 import './App.css';
+
 
 function App() {
 
   const [link, setLink] = useState('')
+  const [qrcodeLink, setQrcodeLink] = useState('')
+
+  function handleGenerate(link_url){
+    QRCodeLink.toDataURL(link_url, {
+      width: 600,
+      margin: 3,
+    }, function (err, url){
+      setQrcodeLink(url);
+    })
+  }
 
   function handleQrcode(event){
     setLink(event.target.value);
+    handleGenerate(event.target.value);
   }
 
   return (
     <div className="container">
-      
-    <QRCode
+
+    <QRCode className="qrcode"
     value={link}
     />
 
@@ -23,6 +36,8 @@ function App() {
       value={link}
       onChange={ (event) => handleQrcode(event)}
       />
+
+      <button className="button"><a href={qrcodeLink} download={`qrcode.png`}>Baixe aqui seu QrCode</a></button>
 
     </div>
   );
